e888c90213053fb89aa3a6bb68c5d0779fff08a3
YXP <954222619@qq.com>
2020-04-04 20:47:20 +0800

    review bubble sort

--- 20180515_04_bubbleSort.cpp
+++ 20180515_04_bubbleSort.cpp
@@ -11,6 +11,28 @@ void bubbleSort(int *arr, int n)
                 std::swap(arr[j], arr[j + 1]);
 }
 
+// 20200404 review
+void bubble_sort(int *arr, int n) {
+  // 小的冒到前面来,从后往前遍历
+  // for (int i = 0; i < n; ++i) {
+  //   for (int j = n - 1; j > i; --j) {
+  //     if (arr[j] < arr[j - 1]) {
+  //       std::swap(arr[j], arr[j - 1]);
+  //     }
+  //   }
+  // }
+
+  // 大的冒到后面，从前往后
+  for (int i = n - 1; i >= 1; --i) {
+    for (int j = 0; j < i; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        std::swap(arr[j], arr[j + 1]);
+      }
+    }
+  }
+}
+
+
 int main()
 {
     int arr[] = {8, 5, 9, 7, 1, 8, 7, 10, 3};
