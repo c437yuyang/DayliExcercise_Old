d81c4b9695d46948f91fc968f6c5257831bb7489
YXP <954222619@qq.com>
2020-04-04 21:40:01 +0800

    review merge sort

--- 20180515_01_mergeSort.cpp
+++ 20180515_01_mergeSort.cpp
@@ -44,6 +44,50 @@ void merge(int *arr, int L, int R) {
   help = nullptr;
 }
 
+
+/*
+//20200404 review
+void merge(int *arr, int L, int R);
+void merge_sort(int *arr, int L, int R);
+void merge_sort(int *arr, int n) { return merge_sort(arr, 0, n - 1); }
+void merge_sort(int *arr, int L, int R) {
+  if (L == R) {
+    return;
+  }
+
+  int mid = L + (R - L) / 2;
+  merge_sort(arr, L, mid);
+  merge_sort(arr, mid + 1, R);
+  merge(arr, L, R);
+}
+
+void merge(int *arr, int L, int R) {
+  if (L == R) {
+    return;
+  }
+  int *help = new int[R - L + 1]();
+  int mid = L + (R - L) / 2;
+  int i = L, j = mid + 1, k = 0;
+  while (i <= mid && j <= R) {
+    help[k++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+  }
+  while (i <= mid) {
+    help[k++] = arr[i++];
+  }
+  while (j <= R) {
+    help[k++] = arr[j++];
+  }
+
+  k = 0;
+  while (k < (R - L + 1)) {
+    arr[L + k] = help[k++];
+  }
+  delete[] help;
+  help = nullptr;
+}
+
+*/
+
 int main() {
   int arr[] = {8, 5, 9, 7, 1, 8, 7, 10, 3};
   int n = sizeof(arr) / sizeof(arr[0]);
